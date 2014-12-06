a7c48b30d0e0eab5f3bfafcb177ae9c659c79d68
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-12-06 20:55:35 +0100

    Implement method for getting n-th prime number

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,5 +1,15 @@
 class Prime
 
+  @@prime_set = []
+
+  def self.at(position)
+    if @@prime_set[position].nil?
+      @@prime_set = Prime.generate_primes(position+1)
+    end
+
+    @@prime_set[position]
+  end
+
   def self.generate_primes(count)
     potential_prime = 2
     prime_set = [potential_prime]
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -10,6 +10,41 @@ describe Prime do
     ]
   end
 
+  describe ".at" do
+    it "returns a Fixnum" do
+      expect(Prime.at(9)).to be_a(Fixnum)
+    end
+
+    it "returns the correct prime" do
+      10.times do
+        random_position = Random.rand(0..(primes_db.size-1))
+        expect(Prime.at(random_position)).to eq(primes_db[random_position])
+      end
+    end
+
+    context "when prime not already generated" do
+      # Reset the cache
+      before(:each) { Prime.class_variable_set :@@prime_set, [] }
+
+      it "generates primes" do
+        expect(Prime).to receive(:generate_primes).and_return(primes_db)
+        Prime.at(9)
+      end
+    end
+
+    context "when prime already generated" do
+      # Reset the cache
+      before(:each) { Prime.class_variable_set :@@prime_set, [] }
+
+      it "gets the prime from the cache" do
+        Prime.at(9) # Generate cache
+
+        expect(Prime).not_to receive(:generate_primes)
+        Prime.at(9)
+      end
+    end
+  end
+
   describe ".generate_primes" do
     it "returns an Array" do
       expect(Prime.generate_primes(10)).to be_a(Array)
