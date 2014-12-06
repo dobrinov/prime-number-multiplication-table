c38ff2e4b26b210a538e71f895cfde81e1d77ad1
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-12-06 19:33:26 +0100

    Implement prime number generation

--- lib/prime.rb
+++ lib/prime.rb
@@ -1,7 +1,18 @@
 class Prime
 
-  def initialize(position)
-    @position = position
+  def self.generate_primes(count)
+    potential_prime = 2
+    prime_set = [potential_prime]
+
+    while prime_set.count < count
+      potential_prime += 1
+
+      unless prime_set.map { |prime| potential_prime % prime == 0 }.any?
+        prime_set << potential_prime
+      end
+    end
+
+    prime_set
   end
 
 end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe Prime do
 
-  let(:prime) { Prime.new }
+  let(:primes_db) do
+    [
+       2,  3,  5,  7, 11,  13,  17,  19,  23,  29,
+      31, 37, 41, 43, 47,  53,  59,  61,  67,  71,
+      73, 79, 83, 89, 97, 101, 103, 107, 109, 113
+    ]
+  end
 
+  describe ".generate_primes" do
+    it "returns an Array" do
+      expect(Prime.generate_primes(10)).to be_a(Array)
+    end
+
+    it "returns prime numbers only" do
+      expect(Prime.generate_primes(primes_db.size)).to eq(primes_db)
+    end
+
+    it "returns the correct number of primes" do
+      10.times do
+        random_count = Random.rand(1..primes_db.size)
+        expect(Prime.generate_primes(random_count).size).to eq(random_count)
+      end
+    end
+  end
 end
