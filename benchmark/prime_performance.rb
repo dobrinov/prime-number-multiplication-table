09c309632b4b13d4f31f2ef00d20f49e5b38483d
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-12-08 00:44:47 +0100

    Improve performance Improve performance by reusing the already generated primes, by taking them from the cache instead of generating them again.

--- benchmark/prime_performance.rb
+++ benchmark/prime_performance.rb
@@ -1,8 +1,22 @@
-require 'benchmark'
 require_relative '../lib/prime.rb'
 
+suite_start_time = Time.now
+
 (0..1000).step(50).each do |count|
-  Benchmark.bm do |x|
-    x.report(count) { Prime.generate_primes(count) }
-  end
+  iteration_start_time = Time.now
+  Prime.at(count)
+  iteration_end_time = Time.now
+
+  puts "%{prime_count} primes \t %{duration} seconds" % {
+    prime_count: count,
+    duration: (iteration_end_time - iteration_start_time).round(3)
+  }
 end
+
+suite_end_time = Time.now
+
+puts "\n"
+
+puts "Total: \t\t %{duration} seconds" % {
+  duration: (suite_end_time - suite_start_time).round(3)
+}
--- lib/prime.rb
+++ lib/prime.rb
@@ -11,8 +11,8 @@ class Prime
   end
 
   def self.generate_primes(count)
-    potential_prime = 2
-    prime_set = [potential_prime]
+    prime_set = @@prime_set.empty? ? [2] : @@prime_set[0, count] # Use cached primes, if available.
+    potential_prime = prime_set.last
 
     while prime_set.count < count
       potential_prime += 1
